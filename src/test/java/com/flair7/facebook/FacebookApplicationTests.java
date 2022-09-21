a1eb9e832ada9aa546fe6babf75045039f08ecf9
Vini Sousa <guilty77@tutanota.com>
2022-09-21 18:49:36 -0300

    feat: adding the project

---
+++ src/test/java/com/flair7/facebook/FacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flair7.facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
